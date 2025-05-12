2d2f46291bb87c5e6b57e299af71bba5fb90eedd
vynnd <nguynhudangvy5.10.1998@gmail.com>
2025-05-12 08:28:21 +0700

    Add identity-service and profile-service
    - Thêm identity-service có sẵn và khởi tạo profile-service.

---
+++ profile-service/target/generated-sources/annotations/com/devteria/profile/mapper/UserProfileMapperImpl.java
@@ -0,0 +1,48 @@
+package com.devteria.profile.mapper;
+
+import com.devteria.profile.dto.request.ProfileCreationRequest;
+import com.devteria.profile.dto.response.UserProfileReponse;
+import com.devteria.profile.entity.UserProfile;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+)
+@Component
+public class UserProfileMapperImpl implements UserProfileMapper {
+
+    @Override
+    public UserProfile toUserProfile(ProfileCreationRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        UserProfile.UserProfileBuilder userProfile = UserProfile.builder();
+
+        userProfile.firstName( request.getFirstName() );
+        userProfile.lastName( request.getLastName() );
+        userProfile.dob( request.getDob() );
+        userProfile.city( request.getCity() );
+
+        return userProfile.build();
+    }
+
+    @Override
+    public UserProfileReponse toUserProfileReponse(UserProfile entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserProfileReponse.UserProfileReponseBuilder userProfileReponse = UserProfileReponse.builder();
+
+        userProfileReponse.id( entity.getId() );
+        userProfileReponse.firstName( entity.getFirstName() );
+        userProfileReponse.lastName( entity.getLastName() );
+        userProfileReponse.dob( entity.getDob() );
+        userProfileReponse.city( entity.getCity() );
+
+        return userProfileReponse.build();
+    }
+}
