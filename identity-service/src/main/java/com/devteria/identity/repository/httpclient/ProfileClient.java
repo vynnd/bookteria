823698c58dd1fb219ca260501d2b77d98623731b
vynnd <nguynhudangvy5.10.1998@gmail.com>
2025-05-13 14:30:03 +0700

    Custom AuthenticationRequestInterceptor
    - Custom AuthenticationRequestInterceptor and using with FeignClient.

--- identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -1,12 +1,14 @@
 package com.devteria.identity.repository.httpclient;
 
+import com.devteria.identity.configuration.AuthenticationRequestInterceptor;
 import com.devteria.identity.dto.request.profile_service.ProfileCreationRequest;
 import com.devteria.identity.dto.response.profile_service.UserProfileResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(name = "profile-service", url = "${app.services.profile}",
+        configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
 
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
