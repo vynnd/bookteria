2d02b710a366412e61ca1e9dec77b33f9be87550
vynnd <nguynhudangvy5.10.1998@gmail.com>
2025-05-16 14:05:31 +0700

    Apply Map/Dictionary-Based Strategy
    - Describe creation time for post entity by apply Map/Dictionary-Based Strategy pattern.

---
+++ post-service/src/main/java/com/devteria/post/service/DateTimeCustomFormatter.java
@@ -0,0 +1,58 @@
+package com.devteria.post.service;
+
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class DateTimeCustomFormatter {
+
+    Map<Long, Function<Instant, String>> strategyMap = new LinkedHashMap<>();
+
+    public DateTimeCustomFormatter() {
+        strategyMap.put(60L, this::formatInSeconds);
+        strategyMap.put(3600L, this::formatInMinutes);
+        strategyMap.put(86400L, this::formatInHours);
+        strategyMap.put(Long.MAX_VALUE, this::formatInDate);
+    }
+
+    public String format(Instant instant){
+        long elapseSeconds = ChronoUnit.SECONDS.between(instant, Instant.now());
+
+        var strategy = strategyMap.entrySet()
+                .stream()
+                .filter(longFunctionEntry -> elapseSeconds < longFunctionEntry.getKey())
+                .findFirst().get();
+
+
+        return strategy.getValue().apply(instant);
+    }
+
+    private String formatInSeconds(Instant instant){
+        long elapseSeconds = ChronoUnit.SECONDS.between(instant, Instant.now());
+        return elapseSeconds + " seconds";
+    }
+
+    private String formatInMinutes(Instant instant){
+        long elapseMinutes = ChronoUnit.MINUTES.between(instant, Instant.now());
+        return elapseMinutes + " minutes";
+    }
+
+    private String formatInHours(Instant instant){
+        long elapseHours = ChronoUnit.HOURS.between(instant, Instant.now());
+        return elapseHours + " hours";
+    }
+
+    private String formatInDate(Instant instant){
+        LocalDateTime localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE;
+        return localDateTime.format(dateTimeFormatter);
+    }
+}
--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -25,6 +25,7 @@ public class PostService {
 
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeCustomFormatter dateTimeFormatter;
 
     public PostResponse createPost(PostRequest postRequest){
 
@@ -37,8 +38,10 @@ public class PostService {
                 .build();
 
         post = postRepository.save(post);
+        var postResponse = postMapper.toPostResponse(post);
+        postResponse.setCreated(dateTimeFormatter.format(Instant.now()));
 
-        return postMapper.toPostResponse(post);
+        return postResponse;
     }
 
     public PageResponse<PostResponse> getMyPosts(int page, int size){
@@ -49,13 +52,18 @@ public class PostService {
         Pageable pageable = PageRequest.of(page -1, size, sort);
 
         var pageData = postRepository.findAllByUserId(userId, pageable);
+        var postList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
 
         return PageResponse.<PostResponse>builder()
                 .currentPage(page)
                 .pageSize(pageData.getSize())
                 .totalPages(pageData.getTotalPages())
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .data(postList)
                 .build();
     }
 }
